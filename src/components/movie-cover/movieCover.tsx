98060c02d44071ee0e9f33d1f32bd17b9d421e84
Cristóbal <cristobalsepulveda1@hotmail.com>
2023-10-02 19:28:13 -0300

    feat: add cover to movieCover component..

--- src/components/movie-cover/movieCover.tsx
+++ src/components/movie-cover/movieCover.tsx
@@ -3,27 +3,30 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export type MovieCoverProps = {
-    id: number,
-    title: string;
-    image: string;
+  id: number,
+  title: string;
+  image: string;
 }
 
 export const MovieCover = ({
-    id,
-    title,
-    image,
+  id,
+  title,
+  image,
 }: MovieCoverProps) => (
-    <article>
-        <Link
-            href={`/movie/${parseMovieTitle(title)}/showtime/0`}
-            aria-description='link to Barbie schedules'
-        >
-            <figure className="relative aspect-[2/3]">
-                <img
-                    src={image}
-                    className='w-full'
-                />
-                {/* <Image
+  <article>
+    <Link
+      href={`/movie/${parseMovieTitle(title)}/showtime/0`}
+      aria-description='link to Barbie schedules'
+    >
+      <figure className="relative aspect-[2/3]">
+        <img
+          src={image}
+          height="318"
+          width="218"
+          alt={`caratura de la pelicula ${title}`}
+          className='w-full'
+        />
+        {/* <Image
                     src={'/images/dracula.jpeg'}
                     alt='dracula cover'
                     width={0}
@@ -31,10 +34,10 @@ export const MovieCover = ({
                     sizes="100vw"
                     style={{ width: '100%', height: 'auto' }}
                 /> */}
-                <figcaption className='text-center p-4'>
-                    {title}
-                </figcaption>
-            </figure>
-        </Link>
-    </article>
+        <figcaption className='text-center p-4'>
+          {title}
+        </figcaption>
+      </figure>
+    </Link>
+  </article>
 );
